ed640d09e0b97f5405867687468cb7029adbc712
Joaquin Cid <joaqcid@gmail.com>
2017-02-12 17:38:22 -0300

    syncing with ionic db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { QueueData } from '../providers/queue-data';
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': 'fb9ef3e5'
+  }
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +17,8 @@ import { QueueData } from '../providers/queue-data';
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { QueueData } from '../../providers/queue-data'
 
-import {Database} from '@ionic/cloud-angular';
+import { Database } from '@ionic/cloud-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -13,17 +14,32 @@ export class HomePage {
 
   queue: any[] = [];
 
-  constructor(public navCtrl: NavController, public queueData: QueueData) {
-    this.queue = [];
-  }
-
-  ionViewDidLoad() {
-    this.queueData.getItems().subscribe((items: any[]) => {
+  constructor(public navCtrl: NavController
+    , public queueData: QueueData
+    , public db: Database
+    , public alertCtrl: AlertController) {
+    this.db.connect();
+    this.db.collection('items').watch().subscribe((items) => {
       this.queue = items;
+    }, (error) => {
+      console.error(error);
     });
   }
 
+  ionViewDidLoad() {
+  }
+
   bookMeNext() {
-    this.queue.push({"username": "ngraziani"});
+    if (this.queue.find(item => item.id == "joaqcid")) {
+      let alert = this.alertCtrl.create({
+        title: 'Oops!',
+        subTitle: 'Your user is already in the queue!',
+        buttons: ['OK']
+      });
+      alert.present();
+    }
+    else {
+      this.db.collection('items').store({ id:"joaqcid", username: "joaqcid", time: Date.now() });
+    }
   }
 }
